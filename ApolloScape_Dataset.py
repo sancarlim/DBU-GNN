85cfc1ef19ca75abc57605a9562703503e3c06bf
sandracl72 <sandra.carrascol@uah.es>
2020-11-09 09:27:59 +0100

    initializing repository

---
+++ ApolloScape_Dataset.py
@@ -0,0 +1,117 @@
+import numpy as np
+import dgl
+import random
+import pickle
+from scipy import spatial
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+os.environ['DGLBACKEND'] = 'pytorch'
+from torchvision import datasets, transforms
+import scipy.sparse as spp
+from dgl.data import DGLDataset
+
+
+class ApolloScape_DGLDataset(torch.utils.data.Dataset):
+    _raw_dir = '/home/sandra/PROGRAMAS/DBU_Graph/data/apollo_train_data.pkl'
+    '''
+    def __init__(self,
+                 url=None,
+                 raw_dir=None,
+                 save_dir=None,
+                 force_reload=False,
+                 verbose=False):
+        super(ApolloScape_DGLDataset, self).__init__(name='apolloscape',
+                                        url=url,
+                                        raw_dir=self._raw_dir,
+                                        save_dir=save_dir,
+                                        force_reload=force_reload,
+                                        verbose=verbose)
+    '''
+    def __init__(self, data_path=None, train_val='train'):
+        self.raw_dir='/home/sandra/PROGRAMAS/DBU_Graph/data/apollo_train_data.pkl'
+        self.train_val=train_val
+        self.process()        
+
+    def load_data(self):
+        with open(self.raw_dir, 'rb') as reader:
+            # Training (N, C, T, V)=(5010, 11, 12, 120), (5010, 120, 120), (5010, 2)
+            [all_feature, self.all_adjacency, self.all_mean_xy]= pickle.load(reader)
+        all_feature=np.transpose(all_feature, (0,3,2,1)) #(N,V,T,C)
+        self.all_feature=torch.from_numpy(all_feature[:,:70,:,:]).type(torch.float32)
+
+
+    def process(self):
+        #process data to graph, labels, and splitting masks
+
+        self.load_data()
+        total_num = len(self.all_feature)
+        '''
+        last_vis_obj=[]   #contains number of visible objects in each sequence of the training, i.e. objects in frame 5
+        #para hacer grafos de tamaño variable
+        for idx in range(len(adj)): 
+            for i in range(len(adj[idx])): 
+                if adj[idx][i,i] == 0:
+                    last_vis_obj.append(i)
+                    break   
+        '''
+        feature_id = [3, 4, 9, 10]  #x,y,heading,[visible_mask]
+        now_history_frame=6
+        object_type = self.all_feature[:,:,:,2].int()  # torch Tensor NxVxT
+        mask_car=np.zeros((total_num,self.all_feature.shape[1],now_history_frame)) #NxVx6
+        for i in range(total_num):
+            mask_car_t=np.array([1  if (j==2 or j==1) else 0 for j in object_type[i,:,5]])
+            mask_car[i,:]=np.array(mask_car_t).reshape(mask_car.shape[1],1)+np.zeros(6) #120x6
+        
+        self.node_features = self.all_feature[:,:,:now_history_frame,feature_id]  #obj type,x,y 6 primeros frames
+        self.node_labels=self.all_feature[:,:,now_history_frame:,3:5] #x,y 6 ultimos frames
+        self.node_features[:,:,:,-1] *= mask_car   #Pongo 0 en feat 11 [mask] a todos los obj visibles no-car
+        self.node_labels[:,:,:,-1] *= mask_car
+        self.output_mask= self.all_feature[:,:,6:,-1]*mask_car #mascara obj (car) visibles en 6º frame (5010,120,6,1)
+        self.output_mask = np.array(self.output_mask.unsqueeze_(-1) )
+
+        #EDGES weights  #5010x120x120[]
+        self.xy_dist=[spatial.distance.cdist(self.node_features[i][:,5,:], self.node_features[i][:,5,:]) for i in range(len(self.all_feature))]  #5010x120x120
+
+        # TRAIN VAL SETS
+        # Remove empty rows from output mask 
+        zero_indeces_list = [i for i in range(len(self.output_mask)) if np.all(np.array(self.output_mask.squeeze(-1))==0, axis=(1,2))[i] == True ]
+
+        id_list = list(set(list(range(total_num))) - set(zero_indeces_list))
+        total_valid_num = len(id_list)
+        ind=np.random.permutation(id_list)
+        train_id_list, val_id_list = id_list[:round(total_valid_num*0.8)], id_list[round(total_valid_num*0.8):]
+
+        #train_id_list = list(np.linspace(0, total_num-1, int(total_num*0.8)).astype(int))
+        #val_id_list = list(set(list(range(total_num))) - set(train_id_list))  
+
+        if self.train_val.lower() == 'train':
+            self.node_features = self.node_features[train_id_list]
+            self.node_labels = self.node_labels[train_id_list]
+            self.all_adjacency = self.all_adjacency[train_id_list]
+            self.output_mask = self.output_mask[train_id_list]
+            self.all_mean_xy = self.all_mean_xy[train_id_list]
+        elif self.train_val.lower() == 'val':
+            self.node_features = self.node_features[val_id_list]
+            self.node_labels = self.node_labels[val_id_list]
+            self.all_adjacency = self.all_adjacency[val_id_list]
+            self.output_mask = self.output_mask[val_id_list]
+            self.all_mean_xy = self.all_mean_xy[val_id_list]
+
+    def __len__(self):
+        return len(self.node_features)
+
+    def __getitem__(self, idx):
+        graph = dgl.from_scipy(spp.coo_matrix(self.all_adjacency[idx][:70,:70])).int()
+        graph = dgl.remove_self_loop(graph)
+        graph = dgl.add_self_loop(graph)
+        distances = [self.xy_dist[idx][graph.edges()[0][i]][graph.edges()[1][i]] for i in range(graph.num_edges())]
+        norm_distances = [(i-min(distances))/(max(distances)-min(distances)) if (max(distances)-min(distances))!=0 else (i-min(distances))/1.0 for i in distances]
+        norm_distances = [1/(i) if i!=0 else 1 for i in distances]
+        graph.edata['w']=torch.tensor(norm_distances, dtype=torch.float32)
+        graph.ndata['x']=self.node_features[idx] #obj type, x, y
+        graph.ndata['gt']=self.node_labels[idx]
+        output_mask = self.output_mask[idx]
+        
+        return graph, output_mask
