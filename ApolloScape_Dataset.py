112fdcf8127df507d5dd2cbdc7363c01d398824b
sandracl72 <sandra.carrascol@uah.es>
2020-11-10 12:00:30 +0100

    fixed some errors in dataset

--- ApolloScape_Dataset.py
+++ ApolloScape_Dataset.py
@@ -29,7 +29,7 @@ class ApolloScape_DGLDataset(torch.utils.data.Dataset):
                                         force_reload=force_reload,
                                         verbose=verbose)
     '''
-    def __init__(self, data_path=None, train_val):
+    def __init__(self, train_val, data_path=None):
         self.raw_dir='/home/sandra/PROGRAMAS/DBU_Graph/data/apollo_train_data.pkl'
         self.train_val=train_val
         self.process()        
@@ -72,7 +72,7 @@ class ApolloScape_DGLDataset(torch.utils.data.Dataset):
         self.output_mask = np.array(self.output_mask.unsqueeze_(-1) )
 
         #EDGES weights  #5010x120x120[]
-        self.xy_dist=[spatial.distance.cdist(self.node_features[i][:,5,:], self.node_features[i][:,5,:]) for i in range(len(self.all_feature))]  #5010x120x120
+        self.xy_dist=[spatial.distance.cdist(self.node_features[i][:,5,:], self.node_features[i][:,5,:]) for i in range(len(self.all_feature))]  #5010x70x70
 
         # TRAIN VAL SETS
         # Remove empty rows from output mask 
@@ -85,7 +85,7 @@ class ApolloScape_DGLDataset(torch.utils.data.Dataset):
 
         #train_id_list = list(np.linspace(0, total_num-1, int(total_num*0.8)).astype(int))
         #val_id_list = list(set(list(range(total_num))) - set(train_id_list))  
-
+        '''
         if self.train_val.lower() == 'train':
             self.node_features = self.node_features[self.train_id_list]
             self.node_labels = self.node_labels[self.train_id_list]
@@ -98,6 +98,7 @@ class ApolloScape_DGLDataset(torch.utils.data.Dataset):
             self.all_adjacency = self.all_adjacency[self.val_id_list]
             self.output_mask = self.output_mask[self.val_id_list]
             self.all_mean_xy = self.all_mean_xy[self.val_id_list]
+        '''
 
     def __len__(self):
         if self.train_val.lower() == 'train':
@@ -106,6 +107,10 @@ class ApolloScape_DGLDataset(torch.utils.data.Dataset):
             return len(self.val_id_list)
 
     def __getitem__(self, idx):
+        if self.train_val.lower() == 'train':
+            idx = self.train_id_list[idx]
+        else:
+            idx = self.val_id_list[idx]
         graph = dgl.from_scipy(spp.coo_matrix(self.all_adjacency[idx][:70,:70])).int()
         graph = dgl.remove_self_loop(graph)
         graph = dgl.add_self_loop(graph)
@@ -113,8 +118,8 @@ class ApolloScape_DGLDataset(torch.utils.data.Dataset):
         norm_distances = [(i-min(distances))/(max(distances)-min(distances)) if (max(distances)-min(distances))!=0 else (i-min(distances))/1.0 for i in distances]
         norm_distances = [1/(i) if i!=0 else 1 for i in distances]
         graph.edata['w']=torch.tensor(norm_distances, dtype=torch.float32)
-        graph.ndata['x']=self.node_features[idx] #obj type, x, y
-        graph.ndata['gt']=self.node_labels[idx]
-        output_mask = self.output_mask[idx]
+        graph.ndata['x']=self.node_features[idx,:70]#self.last_vis_obj[idx]] #obj type, x, y
+        graph.ndata['gt']=self.node_labels[idx,:70]#self.last_vis_obj[idx]]
+        output_mask = self.output_mask[idx,:70]#self.last_vis_obj[idx]]
         
-        return graph, output_mask
+        return graph, output_mask, self.last_vis_obj[idx]
